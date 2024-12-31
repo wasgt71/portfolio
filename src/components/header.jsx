181493468cf9c868284b46910610cf9a91c3bbc4
Tristan <168455597+wasgt71@users.noreply.github.com>
2024-12-30 20:57:29 -0500

    created js animation for header.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,63 @@
+import { useState, useEffect } from "react";
+
 function Header() {
+  const [name, setName] = useState([]);
+  const [lock, setLock] = useState(true);
+  const displayName = () => {
+    const name = [
+      "H",
+      "i",
+      ",",
+      " I'",
+      "m ",
+      "",
+      "T",
+      "r",
+      "i",
+      "s",
+      "t",
+      "a",
+      "n",
+      " W",
+      "a",
+      "s",
+      "s",
+      "i",
+      "l",
+      "y",
+      "n",
+      ".",
+    ];
+    name.forEach((character, index) => {
+      setTimeout(() => {
+        setName((prevName) => [...prevName, character]);
+      }, index * 150);
+    });
+  };
+
+  useEffect(() => {
+    displayName();
+  }, []);
+
   return (
     <>
-      <header>
-        <h1>Hi, I'm Tristan Wassilyn.</h1>
-        <h3 id="proTitle">Full Stack JavaScript Developer</h3>
+      <button onClick={(e) => setLock(false)} id="contact">
+        Contact
+      </button>
+      <header id="header">
+        <h1 id="name">{name}</h1>
+        <h3 className="fade-in" id="portfolio-title">
+          Full Stack Javascript Developer.
+        </h3>
       </header>
+      {!lock && (
+        <>
+          <div id="contact-info">
+            <p id="phone">{number}</p>
+            <p>{email}</p>
+          </div>
+        </>
+      )}
     </>
   );
 }
